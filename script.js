3c3fd713d7db423c861328508240c7230112a5d1
kumaraswamys28 <manojkumbi2803@gmail.com>
2026-02-13 22:57:32 +0530

    integrated the gcp storage and iam

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const { exec } = require("child_process");
+const { Storage } = require("@google-cloud/storage");
+const fs = require("fs");
 const path = require("path");
-const fs= require('fs');
+const mime = require("mime-types");
+
+const storage = new Storage();
+const bucket = storage.bucket(process.env.BUCKET_NAME);
+
+
+const PROJECT_ID=process.env.PROJECT_ID;
 
 async function init() {
   console.log("Initializing with params:");
@@ -11,17 +19,32 @@ async function init() {
         console.log(data.toString());
     });
 
-    p.stderr.on("error", (data) => {
-        console.error(data.toString());
-    });
+   p.stderr.on("data", (data) => {
+  console.error(data.toString());
+});
 
-    p.on("close", async () => {
-        console.log("build complete");
-        const distfolder=path.join(__dirname, 'output', 'dist')
-        const distcontent= fs.readdirSync(distfolder,{recursive:true})
 
-        for(const filepath of distcontent){
-            if(fs.lstatSync(filepath).isDirectory()) continue;
-        }
+   p.on("close", async () => {
+    if (code !== 0) {
+    console.error("Build failed");
+    process.exit(1);
+  }
+  console.log("build complete");
+
+  const distFolder = path.join(__dirname, "output", "dist");
+  const files = fs.readdirSync(distFolder, { recursive: true });
+
+  for (const relativePath of files) {
+    const fullPath = path.join(distFolder, relativePath);
+
+    if (fs.lstatSync(fullPath).isDirectory()) continue;
+
+    await bucket.upload(fullPath, {
+      destination: `__outputs/${PROJECT_ID}/${relativePath}`,
+      contentType: mime.lookup(fullPath) ,
     });
+  }
+
+  console.log("done...");
+});
 }
